e8fb827aefdc95f6f03ec98311b79e5c78aa751f
Gabrielle Dang <hoangabrielle@gmail.com>
2024-01-02 11:41:02 -1000

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Gabrielle Dang on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
