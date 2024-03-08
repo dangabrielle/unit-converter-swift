39be6ef95cf950bf6597652cf3a889288b378f56
dangabrielle <hoangabrielle@gmail.com>
2024-03-08 12:17:14 -1000

    complete unit conversions between km, ft, yd, and mi

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State var enteredData: Double = 0
+    @State var currentUnit = "km"
+    @State var selectedUnit = "ft"
+
+    let unitValues = ["km", "ft", "yd", "mi"]
+    
+    var conversion: String {
+        var conversion: Double = 0
+        
+        if currentUnit == selectedUnit {
+            conversion = enteredData
         }
-        .padding()
+        
+        if currentUnit == "km" && selectedUnit == "ft" {
+            conversion = enteredData * 3280.84
+        } else if currentUnit == "ft" && selectedUnit == "km" {
+            conversion = enteredData / 3280.84
+        }
+        
+        if currentUnit == "km" && selectedUnit == "yd" {
+            conversion = enteredData * 1093.61
+        } else if currentUnit == "yd" && selectedUnit == "km" {
+            conversion = enteredData / 1093.61
+        }
+        
+        if currentUnit == "km" && selectedUnit == "mi" {
+            conversion = enteredData * 0.621371
+        } else if currentUnit == "mi" && selectedUnit == "km" {
+            conversion = enteredData / 0.621371
+        }
+        
+        if currentUnit == "ft" && selectedUnit == "yd" {
+            conversion = enteredData * 0.333333
+        } else if currentUnit == "yd" && selectedUnit == "ft" {
+            conversion = enteredData / 0.333333
+        }
+        
+        if currentUnit == "ft" && selectedUnit == "mi" {
+            conversion = enteredData * 0.000189394
+        } else if currentUnit == "mi" && selectedUnit == "ft" {
+            conversion = enteredData / 0.000189394
+        }
+        
+        if currentUnit == "yd" && selectedUnit == "mi" {
+            conversion = enteredData * 0.000568182
+        } else if currentUnit == "mi" && selectedUnit == "yd" {
+            conversion = enteredData / 0.000568182
+        }
+        
+        return String(format: "%.2f", conversion)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Convert"){
+                    TextField("0.00", value: $enteredData, format: .number)
+                }
+                Picker("Unit", selection: $currentUnit, content: {ForEach(unitValues, id: \.self) {
+                    Text($0)
+                }})
+                .pickerStyle(.segmented)
+                Section("To") {
+                    Text(conversion)
+                }
+                Picker("Unit", selection: $selectedUnit, content: {ForEach(unitValues, id: \.self) {
+                    Text($0)
+                }})
+                .pickerStyle(.segmented)
+            }
+        }
     }
 }
 
